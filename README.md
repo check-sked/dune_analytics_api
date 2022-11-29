f1767be1998e839bfc86b88f19ce6a80ccf731c4
Zack <zack@Zacks-MBP.lan>
2022-11-29 15:38:10 -0500

    base code and readme

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # Dune-Analytics-API
+
+## Description
+
+The Dune-Analytics-API is a command line tool to execute Dune Analytics queries.
+
+## Table of Contents
+
+- [Usage](#usage)
+- [Walkthrough](#walkthrough)
+- [Contributing](#contributing)
+
+## Usage
+
+_How to use the application:_
+
+To use the tool, open the project's root directory in the command line and run: `node api.js {Dune Query ID}`
+
+## Walkthrough
+
+_How to use the application continued:_
+
+INSERT
+
+## Contributing
+
+_Take the following steps to contribute to the project._
+
+INSERT
+
+---
+
+## Questions?
+
+Got questions? Reach me through one of the following channels:
+
+GitHub: [@zapokorny](https://api.github.com/users/zapokorny)
+
+Email: zwpokorny@gmail.com
---
+++ api.js
@@ -0,0 +1,34 @@
+// TO DO: Replace default API key with ours when API drops
+// TO DO: Add Inquirer prompt for query id # and fix error -- change module to 'commonjs' for inquirer to work or maybe //const queryId = require('./inquirer.js');
+// TO DO: Replace default query id # with user response from inquirer prompt
+// TO DO: Figure out what to do with the results of EXECUTE - call this: https://api.dune.com/api/v1/execution/{{execution_id}}/results
+// Set options as a parameter, environment variable, or rc file.
+
+export {};
+const queryID = process.argv[2];
+console.log(queryID); // Check on Query ID passing through
+import { Headers } from "node-fetch";
+import fetch from "node-fetch";
+
+// Insert API Key here
+const meta = {
+  "x-dune-api-key": "YOUR_API_KEY",
+};
+const header = new Headers(meta);
+
+//  Fetch Dune API for Execution Id
+const response = await fetch(
+  `https://api.dune.com/api/v1/query/${queryID}/execute`, // Query ID previously 1258228
+  {
+    method: "POST",
+    headers: header,
+  }
+);
+const body = await response.text();
+
+// Log the returned response
+console.log(body);
+
+// API for query results -- execution_id = body I'm pretty sure
+// Result of this could be text or json
+//"https://api.dune.com/api/v1/execution/{{execution_id}}/results" execution_id = body from console log
