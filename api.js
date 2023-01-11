94b5ee07d618cfb0d62366b01c838b18ecc7cd3f
Check <check.sked@gmail.com>
2023-01-11 12:57:34 -0500

    Added parameters for query execution and updated comments

--- api.js
+++ api.js
@@ -16,18 +16,27 @@ const meta = {
 };
 const header = new Headers(meta);
 
+// Add parameters we would pass to the query
+var params = {
+  query_parameters: {
+    wallet_address: "YOUR_WALLET_ADDRESS",
+  },
+};
+var body = JSON.stringify(params); // Convert params to a string
+
 //  Fetch Dune API for Execution Id
 const response = await fetch(
   `https://api.dune.com/api/v1/query/${queryID}/execute`, // Query ID previously 1258228
   {
     method: "POST",
     headers: header,
+    body: body, // Parameters passed here
   }
 );
-const body = await response.text();
+const response_object = await response.text();
 
 // Log the returned response
-console.log(body);
+console.log(response_object);
 
 // API for query results -- execution_id = body I'm pretty sure
 // Result of this could be text or json
