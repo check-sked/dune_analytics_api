fdd2816bea7c8175f4f84438b382bc7d20eb6190
Check <check.sked@gmail.com>
2023-01-13 08:14:02 -0500

    Used csv-writer package to produce CSV file with results and updated comments

--- api.js
+++ api.js
@@ -1,12 +1,8 @@
-// TO DO: Replace default API key with ours when API drops
-// TO DO: Add Inquirer prompt for query id # and fix error -- change module to 'commonjs' for inquirer to work or maybe //const queryId = require('./inquirer.js');
-// TO DO: Replace default query id # with user response from inquirer prompt
-// TO DO: Figure out what to do with the results of EXECUTE - call this: https://api.dune.com/api/v1/execution/{{execution_id}}/results
-// Set options as a parameter, environment variable, or rc file.
+import { createObjectCsvWriter } from "csv-writer";
 
 export {};
 const queryID = process.argv[2];
-console.log(queryID); // Check on Query ID passing through
+console.log(queryID); // Check that Query ID is passing through
 import { Headers } from "node-fetch";
 import fetch from "node-fetch";
 
@@ -16,7 +12,7 @@ const meta = {
 };
 const header = new Headers(meta);
 
-// Add parameters we would pass to the query
+// Example of adding parameters we would pass to the query. Comment out if not needed.
 var params = {
   query_parameters: {
     wallet_address: "YOUR_WALLET_ADDRESS",
@@ -25,19 +21,33 @@ var params = {
 var body = JSON.stringify(params); // Convert params to a string
 
 //  Fetch Dune API for Execution Id
-const response = await fetch(
-  `https://api.dune.com/api/v1/query/${queryID}/execute`, // Query ID previously 1258228
-  {
-    method: "POST",
-    headers: header,
-    body: body, // Parameters passed here
+try {
+  const response = await fetch(
+    `https://api.dune.com/api/v1/query/${queryID}/execute`, // Query ID previously 1258228
+    {
+      method: "POST",
+      headers: header,
+      body: body, // Parameters passed here. Comment out if not needed.
+    }
+  );
+  if (!response.ok) {
+    throw new Error(response.statusText);
   }
-);
-const response_object = await response.text();
+  const response_object = await response.text();
+  console.log(response_object);
 
-// Log the returned response
-console.log(response_object);
+  // Write requested data to a CSV file
+  const csvWriter = createObjectCsvWriter({
+    path: "response.csv",
+    header: ["response"],
+  });
 
-// API for query results -- execution_id = body I'm pretty sure
-// Result of this could be text or json
-//"https://api.dune.com/api/v1/execution/{{execution_id}}/results" execution_id = body from console log
+  const records = [{ response: response_object }];
+
+  csvWriter.writeRecords(records).then(() => {
+    // Notify if CSV was successfully created
+    console.log("The CSV file was written successfully");
+  });
+} catch (error) {
+  console.error("Error:", error);
+}
